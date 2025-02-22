c0cebdfcc9e89811cb00033036a8067cdd9e1334
Andrew Driver <A.Driver1@uea.ac.uk>
2025-02-22 22:04:21 +0000

    Another test

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 
 // Visitor count route
 app.get("/api/visitorCount", (req, res) => {
-    res.json({ visitorCount: "Test" });
+    res.json({ visitorCount: 99 });
 });
 
 app.listen(PORT, () => {
